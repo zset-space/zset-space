554a5c8c21fc4f463aee4ccf38e551198b7a93a1
C Anthony Risinger <c@anthonyrisinger.com>
2024-12-09 21:07:13 -0600

    Fixup

--- misc/fetch.sh
+++ misc/fetch.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 say() { echo -e "\e[1;32m$@\e[0m"; } >&2
 err() { echo -e "\e[1;31m$@\e[0m"; } >&2
 sep() { echo -e "\n<!-- $1 $2 -->"; }
-get() { curl -s "$URL$1" | sed -r '/^ *== *(Notes|References|Citations) *== *$/,/\Z/d'; }
+get() { curl -s "$URL$1" | sed -r '/^ *== *(Notes|References|Citations) *== *$/,$d'; }
 
 for title in "$@"; do
     say "GET $title"
