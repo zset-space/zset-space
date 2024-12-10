1a0a1bd957001f63c402b643e2b20015462ef9e5
C Anthony Risinger <c@anthonyrisinger.com>
2024-12-09 21:32:48 -0600

    More cleanups.

--- misc/fetch.sh
+++ misc/fetch.sh
@@ -6,12 +6,17 @@ set -euo pipefail
 
 say() { echo -e "\e[1;32m$@\e[0m"; } >&2
 err() { echo -e "\e[1;31m$@\e[0m"; } >&2
-sep() { echo -e "\n<!-- $1 $2 -->"; }
-get() { curl -s "$URL$1" | sed -r '/^ *== *(Notes|References|Citations) *== *$/,$d'; }
+sep() { echo -e "\n<!-- $* -->"; }
+get() {
+    say GET "$1"
+    sep START "$1"
+    curl "$URL$1" \
+        | sed -r '/^ *== *(Notes|References|Citations) *== *$/,$d' \
+        | pandoc -f mediawiki -t gfm --wrap=preserve \
+        || err "$title"
+    sep DONE "$1"
+}
 
 for title in "$@"; do
-    say "GET $title"
-    sep "$title" start
-    get "$title" || err "$title"
-    sep "$title" done
+    get "$title"
 done
